7de17c56a50d4b179cf527a6110dc3a5750e3f0e
bwolski <benwolski@gmail.com>
2018-08-21 16:26:45 -0700

    arbitrary update

--- src/widgets/HelloWorld.ts
+++ src/widgets/HelloWorld.ts
@@ -3,6 +3,6 @@ import { WidgetBase } from "@dojo/framework/widget-core/WidgetBase";
 
 export default class HelloWorld extends WidgetBase {
   protected render() {
-    return v("h1", { title: "I am a title" }, ["Biz-E-Bodies"]);
+    return v("h1", { title: "I am a title" }, ["Bizz-E-Bodies"]);
   }
 }
